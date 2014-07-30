e1a993e34b56ae85a125f81963626f9a7735e0e5
Sanyam Agrawal <sanyam_agrawal@intuit.com>
2014-07-30 10:36:58 +0530

    Requiring app.js which will be the application which will do the rest of the task for us . This whole application will rely on this js file.

--- app/base/app.js
+++ app/base/app.js
@@ -1,1 +1,18 @@
-console.log("Application Inilization To begin Now!! Fasten Your Seat Belt");
+define([
+    "dojo/_base/declare"
+], function(declare) {
+
+    var App = declare(null, {
+
+        constructor : function() {
+            console.log("Reached Constructor");
+        },
+
+        render: function() {
+            console.log("Application Inilization To begin Now!! Fasten Your Seat Belt");
+        }
+
+    });
+
+    return new App();
+});
