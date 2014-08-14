a4b86e78e64fa7dd3f691cd36e2fe21f7d23a2e2
Sanyam Agrawal <sanyam_agrawal@intuit.com>
2014-08-14 11:38:18 +0530

    Moving extra code to example and making the Shell thin . This will breakdown into Navigationpart and the main shell part. Still confused as the shell should only be the white screen or the navigation as well

--- app/shell/ShellController.js
+++ app/shell/ShellController.js
@@ -1,54 +1,19 @@
+/*
+    Shell Class . This is the Overview of the entire shell . A shell is always required by the App .
+    An app can have multiple shells. But the vice versa is not true.
+*/
 define([
     "dojo/_base/declare",
-    "dojo/dom-construct",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dojo/text!./templates/ShellView.html"
-], function(declare, domConstruct, WidgetBase, TemplatedMixin, template) {
+], function(declare, WidgetBase, TemplatedMixin, template) {
 
     return declare([WidgetBase, TemplatedMixin], {
-
-        value: 1,
 
         baseClass : "dojoSPA",
 
         templateString : template,
-
-        constructor: function() {
-            console.log("Reached ShellController" + this.value++);
-            this.inherited(arguments);
-        },
-
-        postscript: function() {
-            console.log("Reach Postscript" + this.value++);
-            this.inherited(arguments);
-        },
-
-        postMixInProperties: function() {
-            console.log("Reached Post Mixin In Properties" + this.value++);
-            this.inherited(arguments);
-        },
-
-        buildRendering: function() {
-            console.log("Reached BuildRendering" + this.value++);
-            this.domNode = domConstruct.create("button", {innerHTML: "push me"});
-            this.inherited(arguments);
-        },
-
-        postCreate: function() {
-            console.log("Reached Post Create" + this.value++);
-            this.inherited(arguments);
-        },
-
-        startup: function() {
-            console.log("Reached Startup" + this.value++);
-            this.inherited(arguments);
-        },
-
-        destroy: function() {
-            console.log("Reached destroy" + this.value++);
-            this.inherited(arguments);
-        }
 
     });
 
